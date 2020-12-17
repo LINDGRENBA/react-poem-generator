f3a9ef0b0969d7624b25b385ed7739f9f53aeafe
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-17 15:55:30 -0800

    Update location of App file, add Poem component.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Poem from './components/Poem';
+
+const App = () => {
+  return (
+    <Poem text='new poem' />
+  );
+};
+
+export default App;
---
+++ src/components/Poem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Poem = ({ text }) => {
+  return (
+    <div>
+          <button>{text}</button>
+    </div>
+  )
+};
+
+export default Poem;
