7f6dea435e0bff5ec9477e908943a2b1026d0364
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-18 22:55:21 -0800

    Install new package, update Poem component with API calls, add hooks.

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,16 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+// import { dotenv } from 'dotenv';
 import axios from 'axios';
 
 const Poem = ({ text }) => {
-  const [poem, setPoem] = useState('Click the "new poem" button to generate a poem');
+  const [poemTitles, setPoemTitles] = useState('');
+  const [currentPoem, setCurrentPoem] = useState('');
+  const [poemUrl, setPoemUrl] = useState('');
+  const [currentTitle, setCurrentTitle] = useState('');
 
+  useEffect(() => {
+    const getPoemTitles = async () => {
+      const titleOptions = {
+        method: "GET",
+        url: "https://pafmon-walt-whitman-poems.p.rapidapi.com/poems/",
+        headers: {
+          "x-rapidapi-host": "pafmon-walt-whitman-poems.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_API_KEY
+        }
+      }
+      const { data } = await axios.request(titleOptions);
+      setPoemTitles(data);
+    }
 
+    getPoemTitles();
+  }, []);
 
+  const onButtonClick = async () => {
+    const randomIndex = Math.floor(Math.random() * 407);
+    const title = poemTitles[randomIndex];
+    setPoemUrl(`https://pafmon-walt-whitman-poems.p.rapidapi.com/poems/${title}`)
+
+    const poemOptions = {
+      method: "GET",
+      url: poemUrl,
+      headers: {
+        "x-rapidapi-host": "pafmon-walt-whitman-poems.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY
+      }
+    }
+    const { data } = await axios.request(poemOptions);
+    setCurrentTitle(data.title);
+    setCurrentPoem(data.text);
+  }
+
+//   data:
+// index: "as-consequent-etc."
+// text: "the poem will be here"
+// title: 
+
+  console.log(poemTitles);
 
   return (
     <div>
-      <p>{poem}</p>
-      <button className="ui button">{text}</button>
+      <h1>{currentTitle}</h1>
+      <p>{currentPoem === '' ? 'Click the "new poem" button to generate a random poem' : currentPoem}</p>
+      <button onClick={onButtonClick}  className="ui button">{text}</button>
     </div>
   )
 };
